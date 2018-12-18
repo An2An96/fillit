6bafae72571df460c3a0b960fa331b7e2e3cc1b8
Rupert brax Schuppe <rschuppe@e2r2p4.21-school.ru>
2018-12-18 16:53:28 +0300

    update getmapsize

--- srcs/getmapsize.c
+++ srcs/getmapsize.c
@@ -6,30 +6,18 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 14:56:05 by anorjen           #+#    #+#             */
-/*   Updated: 2018/12/18 13:36:37 by rschuppe         ###   ########.fr       */
+/*   Updated: 2018/12/18 16:52:47 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	getmapsize(int terms)	//	переписать!
+int	getmapsize(int terms)
 {
 	int	i;
 
-	i = 2;
-	if (terms == 1)
-		return (2);
-	else if (terms == 2)
-		return (3);
-	while (i * i != terms)
-	{
-		if (i < terms / 2)
-			i++;
-		else
-		{
-			terms++;
-			i = 2;
-		}
-	}
-	return (2 * i);
+	i = 3;
+	while (terms * 4 > i * i)
+		i++;
+	return (i);
 }
