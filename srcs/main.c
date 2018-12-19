518ee9cbdd594380c18c5fecc0e34c7168835b5e
Rupert brax Schuppe <rschuppe@e2r2p4.21-school.ru>
2018-12-19 14:49:22 +0300

    update main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 14:35:06 by rschuppe          #+#    #+#             */
-/*   Updated: 2018/12/19 14:44:30 by rschuppe         ###   ########.fr       */
+/*   Updated: 2018/12/19 14:49:14 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int		main(int argc, char **argv)
 {
-	if (argc > 1)
+	if (argc == 2)
 	{
 		if (fillit(argv[1]) == 0)
 			ft_putstr("error\n");
