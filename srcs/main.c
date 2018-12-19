38d6f0e7b16765bbe7c6f19647692e85b6d6d7d4
Rupert brax Schuppe <rschuppe@e2r2p4.21-school.ru>
2018-12-19 14:44:49 +0300

    update output

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rschuppe <rschuppe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/13 14:35:06 by rschuppe          #+#    #+#             */
-/*   Updated: 2018/12/19 14:08:11 by rschuppe         ###   ########.fr       */
+/*   Updated: 2018/12/19 14:44:30 by rschuppe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		main(int argc, char **argv)
 	if (argc > 1)
 	{
 		if (fillit(argv[1]) == 0)
-			ft_putstr("error");
+			ft_putstr("error\n");
 	}
 	else
-		ft_putstr("usage: ./fillit [file]");
+		ft_putstr("usage: ./fillit file");
 	return (0);
 }
